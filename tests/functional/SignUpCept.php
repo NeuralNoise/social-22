b7e14ccd721f9fd6651185d73395e63a1ea63f1e
superstoffer <christoffer@baadsgaard.net>
2014-09-14 18:30:16 +0200

    a test for sign up function

---
+++ tests/functional/SignUpCept.php
@@ -0,0 +1,16 @@
+<?php 
+$I = new FunctionalTester($scenario);
+$I->am('a guest');
+$I->wantTo('Sign up for a Socializer account');
+
+$I->amOnPage('/');
+$I->click('Tilmeld dig');
+$I->seeCurrentUrlEquals('/register');
+
+$I->fillField('Email:', 'christoffer@baadsgaard.net');
+$I->fillField('Password:', '123456');
+$I->fillField('Password (gentag):', '123456');
+$I->click('Tilmeld');
+
+$I->seeCurrentUrlEquals('');
+$I->see('Velkommen til Socializer');
